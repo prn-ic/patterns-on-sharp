ee7abc57ba5e615cd90e18b61dca0032d84f9679
zmqp <domarlaid2@mail.ru>
2023-08-29 00:12:00 +0500

    add strategy pattern

---
+++ src/BehavioralPatterns/Strategy/StrategyRealization.cs
@@ -0,0 +1,57 @@
+﻿namespace Strategy;
+
+public interface ISortAlghoritm
+{
+	public void Sort(int[] array);
+}
+
+public class BubbleSort : ISortAlghoritm
+{
+	public void Sort(int[] array) 
+	{
+		for (int i = 0; i < array.Length; i++)
+		{
+			for (int j = i; j < array.Length; j++)
+			{
+				if (array[j] < array[i])
+				{
+					int temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+}
+
+public class LinearSort : ISortAlghoritm
+{
+	public void Sort(int[] array)
+	{
+		for (int i = 0; i < array.Length - 1; i++)
+        {
+            int maxIndex = i;
+ 
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[maxIndex])
+                {
+                    maxIndex = j;
+                }
+            }
+            int temp = array[maxIndex];
+            array[maxIndex] = array[i];
+            array[i] = temp;
+        }
+	}
+}
+
+public class Sort
+{
+	public ISortAlghoritm SortAlghoritm { private get; set; }
+	public void SortArray(int[] array)
+	{
+		SortAlghoritm.Sort(array);
+	}
+
+}
---
+++ src/BehavioralPatterns/UsagesPatterns/Program.cs
@@ -0,0 +1,39 @@
+﻿using Strategy;
+
+StrategyExample();
+
+void StrategyExample()
+{
+	int[] array = new int[] { 5, 3, 2, 7, 4, 1 };
+
+	Console.WriteLine("=BubbleSort=");
+	Console.WriteLine("Array after: ");
+	PrintArray(array);
+
+	Sort sort = new Sort();
+	sort.SortAlghoritm = new BubbleSort();
+
+	sort.SortArray(array);
+	Console.WriteLine("Array before: ");
+	PrintArray(array);
+
+	array = new int[] { 5, 3, 2, 7, 4, 1 };
+	Console.WriteLine("=LinearSort=");
+	Console.WriteLine("Array after: ");
+	PrintArray(array);
+
+	sort.SortAlghoritm = new LinearSort();
+
+	sort.SortArray(array);
+	Console.WriteLine("Array before: ");
+	PrintArray(array);
+}
+
+void PrintArray(int[] array)
+{
+	foreach (var item in array)
+	{
+		Console.Write(item + " ");
+	}
+	Console.WriteLine();
+}
