b32ea8828ea685dadaca57ab3d2c56254b13be6d
prnic <domarlaid2@mail.ru>
2023-09-08 13:47:32 +0500

    add decorator pattern

---
+++ src/StructuralPatterns/Decorator/DecoratorRealization.cs
@@ -0,0 +1,54 @@
+﻿namespace Decorator;
+
+public abstract class Browser
+{
+    public string Name { get; private set;}
+    public string ExtensionsPath { get; protected set; }
+    public Browser(string name) => Name = name;
+    public Browser(string name, string extension) 
+    {
+        Name = name;
+        ExtensionsPath = extension;
+    } 
+    public abstract string GetExtensions();
+}
+
+public class ChromeBrowser : Browser
+{
+    public ChromeBrowser() : base("Google Chrome") { }
+    public override string GetExtensions() => ExtensionsPath;
+}
+
+public class MozillaBrowser : Browser
+{
+    public MozillaBrowser() : base("Mozilla Firefox") { }
+    public override string GetExtensions() => ExtensionsPath;
+}
+
+public class AmigoBrowser : Browser
+{
+    public AmigoBrowser() : base("Zalupa Ebanaya") { }
+    public override string GetExtensions() => ExtensionsPath;
+}
+
+public abstract class BrowserDecorator : Browser
+{
+    protected Browser browserInstanse;
+    public BrowserDecorator(string name, string extension, Browser browserInstanse) 
+        : base(name, browserInstanse.ExtensionsPath + extension + "; ") =>
+        this.browserInstanse = browserInstanse;
+}
+
+public class AdBlockExtension : BrowserDecorator
+{
+    public AdBlockExtension(Browser browserInstanse) 
+        : base(browserInstanse.Name, "AdBlock", browserInstanse) { }
+    public override string GetExtensions() => browserInstanse.ExtensionsPath;
+}
+
+public class TranslatorExtension : BrowserDecorator
+{
+    public TranslatorExtension(Browser browserInstanse) 
+        : base(browserInstanse.Name, "Translator", browserInstanse) { }
+    public override string GetExtensions() => browserInstanse.ExtensionsPath;
+}
---
+++ src/StructuralPatterns/UsagesPatterns/Program.cs
@@ -0,0 +1,27 @@
+﻿using Decorator;
+
+Dictionary<string, Action> actions = new Dictionary<string, Action>()
+{	
+	{ "Decorator" , DecoratorExample }
+};
+
+PrintResults();
+
+void DecoratorExample()
+{
+    Browser browser = new AmigoBrowser();
+    Console.WriteLine($"Name: {browser.Name}; Extensions: {browser.ExtensionsPath}");
+    browser = new AdBlockExtension(browser);
+    Console.WriteLine($"Name: {browser.Name}; Extensions: {browser.GetExtensions()}");
+    browser = new TranslatorExtension(browser);
+    Console.WriteLine($"Name: {browser.Name}; Extensions: {browser.GetExtensions()}");
+}
+
+void PrintResults()
+{
+	foreach (var action in actions)
+	{
+		Console.WriteLine($"\n====={action.Key}=====\n");
+		action.Value.Invoke();
+	}
+}
