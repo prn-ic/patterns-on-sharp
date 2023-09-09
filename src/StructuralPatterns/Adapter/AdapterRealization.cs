d59c1ae9bc4a8a426086570db248a053cbe814f5
prnic <domarlaid2@mail.ru>
2023-09-09 19:07:19 +0500

    add adapter pattern

---
+++ src/StructuralPatterns/Adapter/AdapterRealization.cs
@@ -0,0 +1,45 @@
+﻿namespace Adapter;
+
+public interface IHdmi
+{
+    void TranslateVideo();
+    void TranslateAudio();
+}
+
+public interface IVga
+{
+    void TranslateVideo();
+}
+
+public class SomeHdmiCabel : IHdmi
+{
+    public void TranslateVideo() =>
+        Console.WriteLine("[✓](V) Транслируем видео через HDMI кабель some бирки");
+    public void TranslateAudio() =>
+        Console.WriteLine("[✓](A) Транслируем аудио через HDMI кабель some марки");
+}
+
+public class SomeVgaCabel : IVga
+{
+    public void TranslateVideo() =>
+        Console.WriteLine("[✓](V) Транслируем видео через VGA кабель some марки");
+}
+
+public class VgaToHdmiAdapter : IHdmi
+{
+    private IVga vga;
+    public VgaToHdmiAdapter(IVga vga) => this.vga = vga;
+    public void TranslateVideo() => 
+        Console.WriteLine("[✓](V) Транслируем видео через адаптер VGA to HDMI кабель");
+    public void TranslateAudio() =>
+        Console.WriteLine("[x](A) VGA to HDMI - Аудио не поддерживается");
+}
+
+public class Tv
+{
+    public void ShowThroughHdmi(IHdmi hdmi)
+    {
+        hdmi.TranslateVideo();
+        hdmi.TranslateAudio();
+    }
+}
--- src/StructuralPatterns/UsagesPatterns/Program.cs
+++ src/StructuralPatterns/UsagesPatterns/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Decorator;
+using Adapter;
 
 Dictionary<string, Action> actions = new Dictionary<string, Action>()
 {	
-	{ "Decorator" , DecoratorExample }
+	{ "Decorator" , DecoratorExample },
+    { "Adapter", AdapterExample }
 };
 
 PrintResults();
@@ -15,6 +17,17 @@ void DecoratorExample()
     Console.WriteLine($"Name: {browser.Name}; Extensions: {browser.GetExtensions()}");
     browser = new TranslatorExtension(browser);
     Console.WriteLine($"Name: {browser.Name}; Extensions: {browser.GetExtensions()}");
+}
+
+void AdapterExample()
+{
+    Tv tv = new Tv();
+    SomeHdmiCabel hdmi = new SomeHdmiCabel();
+    SomeVgaCabel vga = new SomeVgaCabel();
+
+    tv.ShowThroughHdmi(hdmi);
+    // tv.ShowThroughHdmi(vga) - будет ошибка
+    tv.ShowThroughHdmi(new VgaToHdmiAdapter(vga));
 }
 
 void PrintResults()
