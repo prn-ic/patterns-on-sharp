bb5eaa798f952157e6bfcb9e28b08fd5d3822f22
prnic <domarlaid2@mail.ru>
2023-09-12 11:59:10 +0500

    update strategy pattern (formatting text)

--- src/BehavioralPatterns/Strategy/StrategyRealization.cs
+++ src/BehavioralPatterns/Strategy/StrategyRealization.cs
@@ -7,7 +7,7 @@ public interface ISortAlghoritm
 
 public class BubbleSort : ISortAlghoritm
 {
-	public void Sort(int[] array) 
+	public void Sort(int[] array)
 	{
 		for (int i = 0; i < array.Length; i++)
 		{
@@ -29,20 +29,20 @@ public class LinearSort : ISortAlghoritm
 	public void Sort(int[] array)
 	{
 		for (int i = 0; i < array.Length - 1; i++)
-        {
-            int maxIndex = i;
- 
-            for (int j = i + 1; j < array.Length; j++)
-            {
-                if (array[j] < array[maxIndex])
-                {
-                    maxIndex = j;
-                }
-            }
-            int temp = array[maxIndex];
-            array[maxIndex] = array[i];
-            array[i] = temp;
-        }
+		{
+			int maxIndex = i;
+
+			for (int j = i + 1; j < array.Length; j++)
+			{
+				if (array[j] < array[maxIndex])
+				{
+					maxIndex = j;
+				}
+			}
+			int temp = array[maxIndex];
+			array[maxIndex] = array[i];
+			array[i] = temp;
+		}
 	}
 }
 
